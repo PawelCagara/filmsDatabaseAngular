cc01e2712264b38966961fbcda6fc8abb71d2458
PawelCagara <pawel.cagara@talan.com>
2023-08-09 13:17:27 +0200

    added option to add film and view them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilmsComponent } from './films/films.component';
+import { FilmtListComponent } from './film-list/film-list.component';
+import { FilmComponent } from './film/film.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'films',
+    component: FilmtListComponent,
+  },
+  {
+    path: 'films/:id',
+    component: FilmComponent,
+  },
+  {
+    path: 'newFilm',
+    component: FilmsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { FilmsComponent } from './films/films.component';
+import { FilmtListComponent } from './film-list/film-list.component';
+import { FilmComponent } from './film/film.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, FilmsComponent, FilmtListComponent, FilmComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, ReactiveFormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
